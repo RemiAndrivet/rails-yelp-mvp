0a2f84da8a88dd2f7cbb5ef3e598256728ebb000
Remi <remi.andrivet@hotmail.com>
2020-06-10 16:43:53 +0200

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :restaurant_reviews
+  post 'restaurants/:id/reviews/new', to: 'reviews#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORY.sample(1)[0]
+  )
+  restaurant.save!
+end
+puts 'Finished!'
